78c7aea6ff489303070cd53aba9c85ef80ae8907
KazukiHayase <74339461+KazukiHayase@users.noreply.github.com>
2021-07-25 16:04:12 +0900

    全画面で共通のパーツを表示

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,21 @@
 import 'src/style/globals.scss'
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient } from '../utilities/apollo/client';
 import { Header } from '@/components/organisms/header'
+import { Sidebar } from '@/components/organisms/sidebar'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ApolloProvider client={ApolloClient.instance}>
       <Header />
-      <div>
-        <Component {...pageProps} />
+      <div className='root'>
+        <Sidebar />
+        <div className='main'>
+          <Component {...pageProps} />
+        </div>
       </div>
-    </>
+    </ApolloProvider>
   )
 }
 export default MyApp
