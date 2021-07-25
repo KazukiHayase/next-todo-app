952e04de45327d3cd1e2743f924b141390040a8d
KazukiHayase <74339461+KazukiHayase@users.noreply.github.com>
2021-07-25 16:03:36 +0900

    サイドバー作成

---
+++ src/components/organisms/sidebar/index.stories.tsx
@@ -0,0 +1,47 @@
+import React, { ComponentProps, useCallback, useEffect, useState } from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { Sidebar } from './index';
+import { FetchCategoriesDocument } from 'src/graphql/types'
+
+export default {
+  title: 'Organisms/Sidebar',
+  component: Sidebar
+} as Meta;
+
+const Template: Story = (args) => (
+  <Sidebar {...args} />
+)
+
+export const Default = Template.bind({});
+Default.args = {};
+Default.parameters = {
+  apolloClient: {
+    // do not put MockedProvider here.
+    // You can, but its preferred to do it in preview.js
+    mocks: [
+      {
+        request: {
+          query: FetchCategoriesDocument, // queryのApollo.DocumentNodeを指定
+        },
+        result: {
+          data: {
+            "categories": [
+              {
+                "id": 1,
+                "name": "カテゴリ１"
+              },
+              {
+                "id": 2,
+                "name": "カテゴリ２"
+              },
+              {
+                "id": 3,
+                "name": "カテゴリ３"
+              }
+            ]
+          },
+        },
+      },
+    ],
+  },
+};
---
+++ src/components/organisms/sidebar/index.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import Link from 'next/link'
+import { useFetchCategoriesQuery } from "src/graphql/types";
+import { useRouter } from "next/dist/client/router";
+import style from './style.module.scss'
+
+
+export const Sidebar: React.VFC = () => {
+  const { data, loading, error } = useFetchCategoriesQuery();
+  const router = useRouter();
+  const { categoryId } = router.query;
+
+  return (
+    <div className={style.sidebar}>
+      <ul>
+        {data?.categories.map(category => (
+          <li
+            key={category.id}
+            className={[
+              `${category.id}` === categoryId ? style.active : ''
+            ].join(' ')}
+          >
+            <Link href={`/tasks/${category.id}`}>
+              <a>{category.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
